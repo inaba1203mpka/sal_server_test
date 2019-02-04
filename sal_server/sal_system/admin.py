ab83b1b9b5632972b3bca9bc831dfdbbee2ec787
inaba1203mpka <koumpka@gmail.com>
2019-02-04 22:54:25 +0900

    sal_system削除、最初から書き直す

--- sal_server/sal_system/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from sal_system.models import Tbl_Reservation
-
-#admin.site.register(Tbl_Reservation)
-
-class Reservation_admin(admin.ModelAdmin) :
-    list_display = ("id","group","select_date","for_time","f_num") #一覧に出したい項目
-
-admin.site.register(Tbl_Reservation,Reservation_admin)
--- sal_server/sal_system/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SalSystemConfig(AppConfig):
-    name = 'sal_system'
